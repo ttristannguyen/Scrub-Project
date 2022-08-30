ca7a06a63677661b5dee358f09edf43e2ac76864
yliu0430 <yliu0430@student.monash.edu>
2022-08-30 10:31:28 +1000

    PB details files created

---
+++ ProjectBacklogDetails/projectBackDetails.js
@@ -0,0 +1,11 @@
+"use strict"
+
+/**
+ * toStartingLocationPage function
+ * changes current page
+ * @param {None} data 
+ */
+function backToPB()
+{
+    window.location = "ProjectBacklog/projectBacklog.html";
+}
