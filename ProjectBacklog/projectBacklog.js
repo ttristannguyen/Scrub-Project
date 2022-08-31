9e38f5770c5409db4c324c2e220648891d84b5b9
shannonWallis <swal0059@student.monash.edu>
2022-08-31 16:24:22 +1000

    making the edit capability and detailed view

--- ProjectBacklog/projectBacklog.js
+++ ProjectBacklog/projectBacklog.js
@@ -1,15 +1,15 @@
 //This file aims at providing the functionality to add a task in the project backlog page,
 //*This is done by the following steps: 
-// Step 1: User clicks on Add a task button
-// Step 2: This takes the user to a new page called "createYourTask.html"
-// Step 3: This page includes a form-style data input
-// Step 4: user enters their details about the task
-// Step 5: The user presses done when they have entered all the required fields
-// Step 6: The details are then added to local storage object under the KEY "ProjectBacklogItems --> PBIS"
+// Step 1: User clicks on Add a task button DONE
+// Step 2: This takes the user to a new page called "taskCreation.html" DONE
+// Step 3: This page includes a form-style data input DONE
+// Step 4: user enters their details about the task DONE
+// Step 5: The user presses done when they have entered all the required fields DONE
+// Step 6: The details are then added to local storage object under the KEY "ProjectBacklogItems --> PBIS" DONE
 // Step 7: The user is returned to the main Project backlog page, where they can now see a dynammically made div of the project backlog item
-// Step 8: The project backlog item shows the summarised information of priority, story points and tag
+// Step 8: The project backlog item shows the summarised information of priority, story points and tag DONE
 // Step 9: When the project backlog div item is created dynamically, a button is also dynamically created at the bottom right hand corner, this 
-//          button can allow a user to see the detailed view of the  task
+//          button can allow a user to see the detailed view of the  task DONE
 // step 10: When viewing the detailed view of the task, the details are not editable, however, a user should be able to "edit task details" 
 // Step 11: This "edit task details" button takes a user to a pre-filled - editable - form, allowing them to edit any detail in the form that they want. 
 
@@ -81,9 +81,12 @@ function onProjectBacklogLoad()
             button.type = 'button';
             button.innerHTML = 'See/edit details';
             button.className = 'btn-styled';
-        
+            button.id = "" + j + "";
             button.onclick = function() {
-                
+                //I want to be able to click on the button, be taken to a 'detailed view' page, where i can possibly make a change to the task. 
+                //This will edit the task in local storage.
+                createDetailedView()
+                localStorage.setItem('currentTaskId',JSON.stringify(button.id))
             };
             let id = j.toString();
             let container = document.getElementById(id);
@@ -95,4 +98,46 @@ function onProjectBacklogLoad()
 function addTaskOnClick()
 {
     window.location.href = 'taskCreation.html'   
+}
+
+function createDetailedView()
+{
+    window.location.href = "taskDetailsEdit.html"
+}
+
+function detailedViewContent()
+{
+    //Get the local storage items 
+    //Parse the local storage items
+    //Get the item for the currently selected item (via button id?)
+    taskIDString = localStorage.getItem('currentTaskId'); //Gets the ID of the current button (position within the local storage array)
+    array = JSON.parse(localStorage.getItem('projectBacklogItemArray')); //Gets the entire array
+    elementNum = parseInt(taskIDString);
+    currentTask = array[elementNum];
+    document.getElementById('taskName').setAttribute('value',currentTask.taskName);
+    document.getElementById('taskDescription').setAttribute('value',currentTask.taskDescription);
+    document.getElementById('taskType').setAttribute('value',currentTask.taskType);
+    document.getElementById('taskStoryPoint').setAttribute('value',currentTask.taskStoryPoint);
+    document.getElementById('priority').setAttribute('value',currentTask.taskPriority);
+
+    
+
+}
+
+function saveEditedDetails()
+{
+    taskName = document.getElementById("taskName").value 
+    taskDescription = document.getElementById("taskDescription").value 
+    taskType = document.getElementById("taskType").value 
+    taskStoryPoint = document.getElementById("taskStoryPoint").value 
+    taskPriority = document.getElementById("priority").value 
+    let productBackLogItemObj = new projectBacklogItem(taskName,taskDescription,taskType,taskStoryPoint,taskPriority);
+    array = JSON.parse(localStorage.getItem('projectBacklogItemArray'))
+    taskIDString = localStorage.getItem('currentTaskId');
+    elementNum = parseInt(taskIDString);    
+    array[elementNum] = productBackLogItemObj; //FIND AND REPLACE
+    //Need to re-set back into local storage
+    localStorage.setItem('projectBacklogItemArray',JSON.stringify(array));
+    
+
 }
