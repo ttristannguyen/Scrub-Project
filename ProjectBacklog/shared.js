40a4dd458e16bae9d411b50f864224ed1400023e
yliu0430 <yliu0430@student.monash.edu>
2022-09-01 22:38:50 +1000

    1

--- ProjectBacklog/shared.js
+++ ProjectBacklog/shared.js
@@ -1,29 +1,29 @@
 // Predefined keys for LS
 const TASK_KEY = "currentTaskIndex"
 const PBILIST_KEY = "PBIListData"
-class ProjectBacklogItem {
-    constructor(taskName, taskDescription, taskType, taskStoryPoint, taskPriority)
-    {
-        this.taskName = taskName;
-        this.taskDescription = taskDescription;
-        this.taskType = taskType;
-        this.taskStoryPoint = taskStoryPoint;
-        this.taskPriority = taskPriority;
-    }
-    get taskName() {return this.taskName}
-    get taskDescription() {return this.taskDescription}
-    get taskType() {return this.taskType}
-    get taskStoryPoint() {return this.taskStoryPoint}
-    get taskPriority() {return this.taskPriority}
-    fromData(data) {
-        this.taskName = data.taskName;
-        this.taskDescription = data.taskDescription;
-        this.taskType = data.taskType;
-        this.taskStoryPoint = data.taskStoryPoint;
-        this.taskPriority = data.taskPriority;
-    }
+// class ProjectBacklogItem {
+//     constructor(taskName, taskDescription, taskType, taskStoryPoint, taskPriority)
+//     {
+//         this.taskName = taskName;
+//         this.taskDescription = taskDescription;
+//         this.taskType = taskType;
+//         this.taskStoryPoint = taskStoryPoint;
+//         this.taskPriority = taskPriority;
+//     }
+//     get taskName() {return this.taskName}
+//     get taskDescription() {return this.taskDescription}
+//     get taskType() {return this.taskType}
+//     get taskStoryPoint() {return this.taskStoryPoint}
+//     get taskPriority() {return this.taskPriority}
+//     fromData(data) {
+//         this.taskName = data.taskName;
+//         this.taskDescription = data.taskDescription;
+//         this.taskType = data.taskType;
+//         this.taskStoryPoint = data.taskStoryPoint;
+//         this.taskPriority = data.taskPriority;
+//     }
 
-}
+// }
 
 class PBIList {
     constructor() {
