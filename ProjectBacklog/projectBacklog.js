2fb25dd544025dafd04f3a4f06814a767059b111
Stefan Su <ssuu0017@student.monash.edu>
2022-09-16 11:28:05 +1000

    Added memberAssigned attribute to PBI Class

--- ProjectBacklog/projectBacklog.js
+++ ProjectBacklog/projectBacklog.js
@@ -36,6 +36,7 @@ let projectBacklogItem = class {
         this.taskType = taskType;
         this.taskStoryPoint = taskStoryPoint;
         this.taskPriority = taskPriority;
+        this.memberAssigned = null;
     }
 
 }
